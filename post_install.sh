90e68cc7548c1d9592c481b2546d60e74b0dfbc9
Vishal Gupta <vishalgupta7972@gmail.com>
2023-09-19 14:53:40 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,4 @@
-sudo apt install -y vim nano emacs openssh-server gcc g++ gdb git cmake
+sudo apt install -y vim nano openssh-server gcc g++ gdb git cmake
 
 git clone https://github.com/google/googletest.git -b v1.14.0
 cd googletest
